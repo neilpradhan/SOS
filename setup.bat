1efde9340050140c4bb086158653f8a605046ea9
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2025-01-15 15:09:14 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -7,7 +7,7 @@ REM Open terminal for decryption_server with venv creation and activation
 start cmd /k "cd decryption_server && (if not exist .venv python -m venv .venv) && .venv\Scripts\activate && pip install -r requirements.txt"
 
 REM Open terminal for encryption-app
-start cmd /k "cd encryption-app && npm install"
+start cmd /k "cd encryption_app && npm install"
 
 REM Open terminal for decryption-app
-start cmd /k "cd decryption-app && npm install"
+start cmd /k "cd decryption_app && npm install"
