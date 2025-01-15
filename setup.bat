2f9c8fdfb7a9f3cb1e83d7168e64016105125c30
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2025-01-15 13:45:34 +0100

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Open terminal for encryption_server with venv creation and activation
+start cmd /k "cd encryption_server && (if not exist .venv python -m venv .venv) && .venv\Scripts\activate && pip install -r requirements.txt"
+
+REM Open terminal for decryption_server with venv creation and activation
+start cmd /k "cd decryption_server && (if not exist .venv python -m venv .venv) && .venv\Scripts\activate && pip install -r requirements.txt"
+
+REM Open terminal for encryption-app
+start cmd /k "cd encryption-app && npm install"
+
+REM Open terminal for decryption-app
+start cmd /k "cd decryption-app && npm install"
